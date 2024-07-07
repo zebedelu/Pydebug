e834e531f042c81dfc2379530bb99146d107dc34
pedro961247 <129136825+pedro961247@users.noreply.github.com>
2024-07-06 23:44:05 -0300

    Add files via upload

--- compile exe/RECOMPILE.bat
+++ compile exe/RECOMPILE.bat
@@ -1,3 +1,9 @@
+@echo off
+set /p name=Insira o nome do arquivo (sem conter aspas):
 windres resources.rc -o resources.o
-g++ -o "../python compilator.exe" "../python compilator.cpp" resources.o
+
+rem Caso de erro acontecer irá tentar achar os arquivos no mesmo diretório do recompile.bat
+
+g++ -o "../%name%.exe" "../%name%.cpp" resources.o || ( g++ -o "%name%.exe" "%name%.cpp" resources.o )
+
 timeout 1
